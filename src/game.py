78469428f3eade424b6b3e6ecafccc7064e67e08
OddKid123 <luna.rose.poland@gmail.com>
2023-04-08 18:55:08 -0400

    Fixed gravity and player can move left and right.

--- src/game.py
+++ src/game.py
@@ -26,6 +26,7 @@ player_speed = 5
 player_jump_speed = 12
 player_jump_height = 200
 player_rect = pygame.Rect(player_x, player_y, player_size, player_size)
+player_velocity = [0, 0]
 
 # set up platform properties
 platform_width = 100
@@ -57,79 +58,39 @@ while running:
             if event.key == pygame.K_SPACE and not jumping:
                 jumping = True
                 jump_counter = 0
+                player_velocity[1] = -player_jump_speed
 
-
-    # Player movement
-    player_y_change = 0
-
-    if event.type == pygame.KEYDOWN:
-        if event.key == pygame.K_SPACE:
-            # Set player_y_change to a positive value to simulate gravity pulling the player down
-            player_y_change = 8
-    elif event.type == pygame.KEYUP:
-        if event.key == pygame.K_SPACE:
-            # Gradually decrease player_y_change to slow down player's upward motion and reverse direction
-            for i in range(player_y_change):
-                player_y_change -= 1
-
-    player_y += player_y_change
+    # handle movement
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_a]:
+        player_velocity[0] = -player_speed
+    elif keys[pygame.K_d]:
+        player_velocity[0] = player_speed
+    else:
+        player_velocity[0] = 0
 
     # handle jumping
     if jumping:
-        player_rect.move_ip(0, -player_jump_speed)
-        jump_counter += player_jump_speed
+        jump_counter += abs(player_velocity[1])
         if jump_counter >= player_jump_height:
             jumping = False
 
     # handle gravity
     if not jumping:
-        player_rect.move_ip(0, gravity)
-        for platform_rect in platform_rects:
-            if player_rect.colliderect(platform_rect):
-                if gravity > 0:
-                    player_rect.bottom = platform_rect.top
-                    jumping = False
-                else:
-                    player_rect.top = platform_rect.bottom
-                break
+        player_velocity[1] += gravity
+    else:
+        player_velocity[1] += gravity / 2
 
-    # handle platform movement
-    for platform_rect in platform_rects[1:]:
-        platform_rect.move_ip(-platform_speed, 0)
-        if platform_rect.right < 0:
-            platform_rect.left = WINDOW_WIDTH
+    # move player
+    player_rect.move_ip(player_velocity[0], player_velocity[1])
 
-    # draw game objects
-    screen.blit(background_image, (0, 0))
-    pygame.draw.rect(screen, DARK_PINK, platform_rects[0])
-    for platform_rect in platform_rects[1:]:
-        pygame.draw.rect(screen, BROWN, platform_rect)
-    pygame.draw.rect(screen, WHITE, player_rect)
-
-    # handle jumping
-    if jumping:
-        player_rect.move_ip(0, -player_jump_speed)
-        jump_counter += player_jump_speed
-        if jump_counter >= player_jump_height:
+    # handle collisions
+    for platform_rect in platform_rects:
+        if player_rect.colliderect(platform_rect) and player_velocity[1] > 0:
+            player_rect.bottom = platform_rect.top
+            player_velocity[1] = 0
             jumping = False
-
-    # handle gravity
-    if not jumping:
-        player_rect.move_ip(0, gravity)
-        on_platform = False
-        for platform_rect in platform_rects:
-            if player_rect.colliderect(platform_rect):
-                on_platform = True
-                if gravity > 0:
-                    player_rect.bottom = platform_rect.top
-                else:
-                    player_rect.top = platform_rect.bottom
-                break
-
-        if not on_platform:
-            gravity += 0.5
-        else:
-            gravity = 0.5
+            break
 
     # handle platform movement
     for platform_rect in platform_rects[1:]:
@@ -148,6 +109,6 @@ while running:
     pygame.display.update()
     clock.tick(60)
 
-# quit pygame and exit program
+#quit pygame and exit program
 pygame.quit()
 exit()
